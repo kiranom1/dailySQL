d78c8867041c727a9bf55bab61d94cfc8a18901d
unknown <kiran.om@gmail.com>
2023-10-16 18:01:25 +0100

    Monday 16.October.2023 18:01:25

--- pracSQL.sql
+++ pracSQL.sql
@@ -24,3 +24,35 @@ ALTER TABLE "tblContractors" ADD COLUMN "contractor_designation" text
 /* Drop-delete a column in the table */
 ALTER TABLE "tblContractors" DROP COLUMN "is_manager"
 ;
+
+CREATE SEQUENCE IF NOT EXISTS "r"
+	start 675
+	increment 3
+;
+CREATE TABLE IF NOT EXISTS "t"
+(
+	  "roll_number" integer PRIMARY KEY DEFAULT nextval('"r"')
+	, "student_name" varchar(90)
+)
+;
+
+ALTER SEQUENCE "r" OWNED BY "t"."roll_number";
+
+INSERT INTO "t" ("student_name") VALUES
+	  ('o')
+	, ('m')
+	, ('k')
+;
+
+ALTER TABLE "t" ALTER COLUMN "roll_number" DROP DEFAULT
+;
+
+CREATE TABLE IF NOT EXISTS "tblEmployees16"
+(
+	  "id" integer
+	, "name" text
+	, "age" int
+	, "is_manager" boolean
+	, "salary" integer
+)
+;
